5e0210bf030149145502a2d8e30d1312a2f69e96
Andrew <mylifescrazy.phifer19@gmail.com>
2025-01-03 15:57:35 -0600

    drafted install and run scripts

---
+++ installer.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/sh
+#please run this from the project root directory
+originalDirectory="$(pwd)"
+
+#load dependencies
+sudo apt install openjdk-8 npm
+
+cd binaries/
+wget -O "json-20241224.jar" https://search.maven.org/remotecontent?filepath=org/json/json/20241224/json-20241224.jar
+cd "$originalDirectory"
+
+
+#build project
+javac -d  "binaries/" -cp "/jar-dependencies/json-20241224.jar" "backend/main.java"
+
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/sh
+java -cp "/jar-dependencies/json-20241224.jar" "/binaries/main"

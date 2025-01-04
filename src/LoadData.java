123c3e7fe73b355f289cd38c94afc4b8ce144fc5
Andrew <mylifescrazy.phifer19@gmail.com>
2025-01-04 12:42:10 -0600

    initial draft of the re-usable card stack loader.  needs some work to handle messy folders.

---
+++ src/LoadData.java
@@ -0,0 +1,44 @@
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+
+public class LoadData {    
+    private Parser pLink;
+    private HtmlRenderer hLink;
+    private JSONHandler sLink;
+
+    public LoadData(String folder)
+    {
+        pLink = Parser.builder().build();
+        hLink = HtmlRenderer.builder().build();
+        sLink = new JSONHandler();        
+    }
+
+
+    public CardStack loadCards(String targetFolder) throws Exception
+    {//TODO: need to add additional processing to make sure this only handles json files, and nothing else!
+        //search targetFolder        
+        File directory = new File(targetFolder);
+        String[] localFiles = directory.list();
+        String[] normalCards = new String[localFiles.length - 1];
+        String topicString = "";
+        int tracker = 0;
+
+        for(int index = 0; index < localFiles.length; index++)
+        { //extract the topic file and seperate it from all the other files
+            String element = localFiles[index];
+            if(element.contains("topic.json"))
+            {topicString = new String(Files.readAllBytes((new File(element)).toPath()), StandardCharsets.UTF_8);}  //find some neat way to reuse this?
+            else
+            {
+                normalCards[tracker] = element;
+                tracker++;
+            }
+        }
+
+        return(new CardStack(topicString, normalCards, pLink, hLink, sLink));
+    }
+}

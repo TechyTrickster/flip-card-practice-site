f36098a0b2af7bcf006a51ddd506af135bc1437e
Andrew <mylifescrazy.phifer19@gmail.com>
2025-01-03 19:52:35 -0600

    initial draft of the serialization and deserialization code.  need to implement the regex sequences.

---
+++ src/JSONHandler.java
@@ -0,0 +1,68 @@
+class SerialHandler {
+    private ReplacementRule[] rules;
+
+
+    public SerialHandler(String[] regexList, String[] replacementStrings) throws Exception
+    {
+        
+        if(regexList.length != replacementStrings.length)
+        {throw new Exception("mismatching rule list lenghts!");}
+        
+        rules = new ReplacementRule[regexList.length];
+        for(int index = 0; index < regexList.length; index++)
+        {rules[index] = new ReplacementRule(regexList[index], replacementStrings[index]);}
+    }
+
+    public String applyRules(String input)
+    {
+        String buffer = input;
+        String output = "";
+
+        for(int index = 0; index < rules.length; index++)
+        {buffer = rules[index].execute(buffer);}
+        
+        return(output);
+    }
+}
+
+
+public class JSONHandler
+{
+    private SerialHandler serialize;
+    private SerialHandler deserialize;
+
+
+    public JSONHandler()
+    {
+        String[] serialRules = {""};
+        String[] serialReplacements = {};
+        String[] deserialRules = {};
+        String[] deserialReplacements = {};
+        try
+        {
+            serialize = new SerialHandler(serialRules, serialReplacements);
+            deserialize = new SerialHandler(deserialRules, deserialReplacements);
+        }
+        catch(Exception e)
+        {System.out.println("IMPOSSIBLE!");}  //this exception shouldn't be reachable unless the embedded constants are wrong.
+        
+    }
+
+
+    public String serialize(String input)  {return(serialize.applyRules(input));}
+    public String deserialize(String input)  {return(deserialize.applyRules(input));}
+}
+
+class ReplacementRule
+{
+    private String matchingRegex; 
+    private String replacementConstant;    
+
+    public ReplacementRule(String matchOn, String replaceWith)
+    {
+        matchingRegex = matchOn;
+        replacementConstant = replaceWith;
+    }
+
+    public String execute(String input)  {return(input.replaceAll(matchingRegex, replacementConstant));}
+}
